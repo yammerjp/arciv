75e463bd37158ecd13d47057206d794139818d24
Keisuke Nakayama <baskk4.dt@gmail.com>
2021-02-26 21:03:57 +0900

    Simplify the subcommand, restore to use stash and unstash

--- commands/commands_restore.go
+++ commands/commands_restore.go
@@ -3,8 +3,6 @@ package commands
 import (
 	"errors"
 	"github.com/spf13/cobra"
-	"os"
-	"path/filepath"
 )
 
 var (
@@ -68,6 +66,7 @@ func restoreAction(args []string) (err error) {
 		return errors.New("Directory structure is not saved with latest commit")
 	}
 
+	// filter blob hashes to recieve
 	localHashStrings, err := selfRepo.FetchBlobHashes()
 	if err != nil {
 		return err
@@ -75,7 +74,6 @@ func restoreAction(args []string) (err error) {
 	for _, lPhoto := range localCommit.Photos {
 		localHashStrings = append(localHashStrings, lPhoto.Hash.String())
 	}
-	// filter blob hashes to recieve
 	var blobsToRecieve []Photo
 	for _, rPhoto := range remoteCommit.Photos {
 		if !isIncluded(localHashStrings, rPhoto.Hash.String()) {
@@ -89,93 +87,22 @@ func restoreAction(args []string) (err error) {
 		for _, b := range blobsToRecieve {
 			message("Download: " + b.Hash.String())
 		}
-	} else {
-		err = remoteRepo.ReceiveRemoteBlobs(blobsToRecieve)
-		if err != nil {
-			return err
-		}
-	}
-
-	// mv all local files to .arciv/blob
-	os.MkdirAll(selfRepo.Path+"/.arciv/blob", 0777)
-	for _, lPhoto := range localCommit.Photos {
-		from := selfRepo.Path + "/" + lPhoto.Path
-		to := selfRepo.Path + "/.arciv/blob/" + lPhoto.Hash.String()
-		if dryRun {
-			message("move " + from + " -> %s" + to)
-			continue
-		}
-		err = os.Rename(from, to)
-		if err != nil {
-			return err
-		}
-		message("moved " + from + " -> %s" + to)
-	}
-
-	// remove garbages
-	if !dryRun {
-		paths, err := findPathsOfSelfRepo(true, true)
-		if err != nil {
-			return err
-		}
-		for i := len(paths) - 1; i >= 0; i-- {
-			err = os.Remove(paths[i])
-			if err != nil {
-				return err
-			}
-		}
-	}
-
-	// rename
-	for _, rPhoto := range remoteCommit.Photos {
-		from := selfRepo.Path + "/.arciv/blob/" + rPhoto.Hash.String()
-		to := selfRepo.Path + "/" + rPhoto.Path
-		if dryRun {
-			message("move " + from + " -> " + to)
-			continue
-		}
-		err = os.Rename(from, to)
-		if err != nil {
-			// mkdirAll and retry
-			err = os.MkdirAll(filepath.Dir(to), 0777)
-			if err != nil {
-				return err
-			}
-			err = os.Rename(from, to)
-			if err != nil {
-				return err
-			}
-		}
-		message("moved " + from + " -> " + to)
-	}
-
-	if dryRun {
 		return nil
 	}
-
-	// remove local .arciv/blob/*
-	localHashStrings, err = selfRepo.FetchBlobHashes()
+	err = remoteRepo.ReceiveRemoteBlobs(blobsToRecieve)
 	if err != nil {
 		return err
 	}
-	for _, blob := range localHashStrings {
-		err = os.Remove(selfRepo.Path + "/.arciv/blob/" + blob)
-		if err != nil {
-			return err
-		}
-		message("removed " + selfRepo.Path + "/.arciv/blob/" + blob)
+
+	// mv all local files to .arciv/blob
+	err = stashPhotos(localCommit.Photos)
+	if err != nil {
+		return err
 	}
+
+	// rename and copy
+	err = unstashPhotos(remoteCommit.Photos)
 	return nil
-	//   - restoreするcommitに必要なhashリストを確認
-	// - 現在のディレクトリ構成とファイルが既にcommitされたどれかに一致することを確認 (誤った上書きを避ける) (status) --force でスキップ
-	// - store先のhashリストを取得
-	//   - storeと一緒
-	// - .arciv/blob/ 以下にあるファイルをリスト化する
-	//   - storeと一緒
-	// - 手元のファイルをリネームして、.arciv/blob/以下含め手元に足りないものをダウンロード
-	//   - restore する commit にあるhashリストのうち、手元のファイルと手元の .arciv/blob に無いファイルを、手元の.arciv/blobにダウンロード
-	//   - 手元のファイルをリネーム
-	//   - .arciv/blob のファイルをリネーム
 }
 
 func init() {
