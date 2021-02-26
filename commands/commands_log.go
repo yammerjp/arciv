7473ab6750f7d1924c90bd4d345468d6700ca826
Keisuke Nakayama <baskk4.dt@gmail.com>
2021-02-26 23:12:01 +0900

    Update commandline options of the subcommand 'log'

--- commands/commands_log.go
+++ commands/commands_log.go
@@ -1,7 +1,6 @@
 package commands
 
 import (
-	"errors"
 	"github.com/spf13/cobra"
 )
 
@@ -9,8 +8,12 @@ var (
 	logCmd = &cobra.Command{
 		Use: "log",
 		Run: logCommand,
+    Args: cobra.NoArgs,
 	}
 )
+
+var repoName string
+var commitAlias string
 
 func logCommand(cmd *cobra.Command, args []string) {
 	if err := logAction(args); err != nil {
@@ -18,38 +21,31 @@ func logCommand(cmd *cobra.Command, args []string) {
 	}
 }
 
-func logAction(args []string) (err error) {
-	switch len(args) {
-	case 0:
-		return printTimeline(SelfRepo())
-	case 1:
-		repo, err := findRepo(args[0])
-		if err == nil {
-			return printTimeline(repo)
-		}
-		// FIXME: check error type
-		commit, err := SelfRepo().LoadCommitFromAlias(args[0])
-		if err != nil {
-			return err
-		}
-		return printCommit(commit)
-	case 2:
-		repo, err := findRepo(args[0])
-		if err != nil {
-			return err
-		}
-		commit, err := repo.LoadCommitFromAlias(args[1])
-		if err != nil {
-			return err
-		}
-		return printCommit(commit)
-	default:
-		return errors.New("Usage: arciv log ([repository-name]) ([commit-alias])")
-	}
-}
-
 func init() {
 	RootCmd.AddCommand(logCmd)
+  logCmd.Flags().StringVarP(&repoName, "repository", "r", "", "repository name")
+  logCmd.Flags().StringVarP(&commitAlias, "commit", "c", "", "commit id")
+}
+
+func logAction(args []string) (err error) {
+  var repo Repository
+  if repoName == "" {
+    repo = SelfRepo()
+  } else {
+    repo, err = findRepo(repoName)
+    if err != nil {
+      return err
+    }
+  }
+
+  if commitAlias == "" {
+    return printTimeline(repo)
+  }
+  commit, err := repo.LoadCommitFromAlias(commitAlias)
+  if err != nil {
+    return err
+  }
+  return printCommit(commit)
 }
 
 func printTimeline(repo Repository) error {
