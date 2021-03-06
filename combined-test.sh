7bcb1d154d4acec43adef8048705c4ae5a891476
Keisuke Nakayama <baskk4.dt@gmail.com>
2021-03-06 22:16:41 +0900

    Add combined test

---
+++ combined-test.sh
@@ -0,0 +1,61 @@
+#!/bin/bash -e
+
+# The shell script is the combined-test of arciv
+# Unit tests is implemented by golang
+
+if ! command -v sha256sum; then
+  echo "Need 'sha256sum'. excute '\$brew install coreutils' if your computer is macOS"
+  exit 1
+fi
+
+# Build
+project_dir="$(cd $(dirname "$0"); pwd)"
+cd "${project_dir}"
+
+go build
+arciv_bin="${project_dir}/arciv"
+
+# Initialize directories
+testing_tmp_dir="/tmp/arciv-combined-test"
+local_repo_dir="${testing_tmp_dir}/local-repo"
+remote_repo_dir="${testing_tmp_dir}/remote-repo"
+rm -rf /tmp/arciv-combined-test
+mkdir -p ${local_repo_dir}
+cd ${local_repo_dir}
+
+# Add files to the local repository
+cp -r "${project_dir}/commands" "${local_repo_dir}"
+
+# Snapshot
+find "${local_repo_dir}" -type f -print0 | xargs -0 sha256sum > "${testing_tmp_dir}/beforeStore"
+
+# Initialize a local repository
+${arciv_bin} init
+
+${arciv_bin} repository add remote-repo "file://${remote_repo_dir}"
+
+# Store blobs to a remote repository
+${arciv_bin} store remote-repo
+
+# Delete files of the local repository
+mv "${local_repo_dir}/.arciv" "${testing_tmp_dir}/.arciv"
+rm -rf "${local_repo_dir}"
+mkdir "${local_repo_dir}"
+mv "${testing_tmp_dir}/.arciv" "${local_repo_dir}/.arciv"
+
+cd ${local_repo_dir}
+# Restore blobs from the remote repository to the local repository 
+${arciv_bin} restore remote-repo "$(${arciv_bin} log --repository remote-repo)" -f
+
+# Remove .arciv
+rm -rf "${local_repo_dir}/.arciv"
+
+# Snapshot
+find "${local_repo_dir}" -type f -print0 | xargs -0 sha256sum > "${testing_tmp_dir}/afterRestore"
+
+# Compare shapshots
+if diff "${testing_tmp_dir}/beforeStore" "${testing_tmp_dir}/afterRestore"; then
+  echo "success"
+else 
+  echo "failed"
+fi
