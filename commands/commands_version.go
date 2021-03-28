74881e75058628f32ff60652d52d85c3591d59d7
Keisuke Nakayama <baskk4.dt@gmail.com>
2021-03-29 01:45:23 +0900

    Add a subcommand 'version'

---
+++ commands/commands_version.go
@@ -0,0 +1,24 @@
+package commands
+
+import (
+  "fmt"
+	"github.com/spf13/cobra"
+)
+
+const versionStr = "0.0.1"
+
+var (
+	versionCmd = &cobra.Command{
+		Use:   "version",
+		Run:   func(_ *cobra.Command, _ []string) {
+      fmt.Println(versionStr)
+    },
+		Short: "Print binary version",
+		Long: "Print binary version",
+		Args: cobra.NoArgs,
+	}
+)
+
+func init() {
+	RootCmd.AddCommand(versionCmd)
+}
